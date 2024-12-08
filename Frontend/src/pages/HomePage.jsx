946fc2f9e680a378c56b3fe6025456f1430b6e2e
yashtiwari912 <yashtiwari92003@gmail.com>
2024-12-09 00:24:25 +0530

    github api integrated

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,17 +1,90 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import toast from "react-hot-toast";
 import ProfileInfo from "../components/ProfileInfo";
 import Repos from "../components/Repos";
 import Search from "../components/Search";
 import SortRepos from "../components/SortRepos";
+import Spinner from "../components/Spinner"
+
 const HomePage = () => {
+    const [userProfile, setUserProfile] = useState(null);
+    const [repos, setRepos] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const [sortType, setSortType] = useState("recent");
+
+    const getUserProfileAndRepos = useCallback(async (username = "yashtiwari912") => {
+        setLoading(true);
+        try {
+            //60 requests per hour,5000 requests for authenticated requests
+            const userRes = await fetch(`https://api.github.com/users/${username}`,
+                {
+                    headers: {
+                        authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`
+                    },
+
+                });
+            const userProfile = await userRes.json();
+            setUserProfile(userProfile);
+            //Repos
+            const repoRes = await fetch(userProfile.repos_url);
+            const repos = await repoRes.json();
+            repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));//descending order,recent first
+            setRepos(repos);
+
+            console.log("userProfile: ", userProfile);
+            console.log("userRepos: ", repos);
+
+            return { userProfile, repos };
+
+        } catch (error) {
+            toast.error(error.message);
+        }
+        finally {
+            setLoading(false);
+        }
+    }, []);
+    //Run only once (when user mounts)
+    useEffect(() => {
+        getUserProfileAndRepos();
+    }, [getUserProfileAndRepos])
+
+    const onSearch = async (e, username) => {
+        e.preventDefault();
+        setLoading(true);
+        setRepos([]);
+        setUserProfile(null);
+
+        const { userProfile, repos } = await getUserProfileAndRepos(username);
+
+        setUserProfile(userProfile);
+        setRepos(repos);
+        setLoading(false);
+        setSortType("recent");
+    }
+
+    const onSort = (sortType) => {
+        if (sortType === "recent") {
+            repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));//descending order,recent first
+        }
+        else if (sortType === "stars") {
+            repos.sort((a, b) => b.stargazers_count - a.stargazers_count);//most stars first
+        }
+        else if (sortType === "forks") {
+            repos.sort((a, b) => b.forks_count - a.forks_count);//most forks first
+        }
+        setSortType(sortType);
+        setRepos([...repos]);
+    }
+
     return (
         <div className='m-4'>
-            <Search />
-            <SortRepos />
+            <Search onSearch={onSearch} />
+            {repos.length > 0 && <SortRepos onSort={onSort} sortType={sortType} />}
             <div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
-                <ProfileInfo />
-                <Repos />
-
+                {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
+                {!loading && <Repos repos={repos} />}
+                {loading && <Spinner />}
             </div>
         </div>
     );
