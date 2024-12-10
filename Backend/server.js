d3c1b1ccf777aba7273677565fa15a59878a3300
yashtiwari912 <yashtiwari92003@gmail.com>
2024-12-10 19:27:37 +0530

    Deployment ready

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import "./passport/github.auth.js"
+import path from "path";
 
 import userRoutes from "./routes/user.route.js"
 import exploreRoutes from "./routes/explore.route.js"
@@ -21,18 +22,27 @@ app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: fals
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(cors());
-const port = 5000;
+//app.use(cors());//Not Needed in Production because the frontend and backend are on the same domain.
+const PORT = process.env.PORT || 5000;
+//making server run with frontend
+const __dirname = path.resolve();
+console.log("dirname: ",__dirname);
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/explore",exploreRoutes);
 
+//making server run with frontend
+app.use(express.static(path.join(__dirname,"/Frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+});
 
 
-app.listen(port, () =>{
-     console.log(`Example app listening on port ${port}!`);
+app.listen(PORT, () =>{
+     console.log(`Server Started on  http://localhost:${PORT}`);
      connectMongoDB();
 })
